92d6f91b286e4da02ac1c3255fe7dd8b45ba98e9
Joel Natividad <1980690+jqnatividad@users.noreply.github.com>
2025-11-29 18:39:28 -0500

    chore: rustfmt

--- src/minmax.rs
+++ src/minmax.rs
@@ -73,7 +73,7 @@ impl<T: PartialOrd + Clone> MinMax<T> {
         // Update min/max - only clone when actually updating
         let needs_min_update = self.min.as_ref().is_none_or(|v| &sample < v);
         let needs_max_update = !needs_min_update && self.max.as_ref().is_none_or(|v| &sample > v);
-        
+
         if needs_min_update {
             self.min = Some(sample.clone());
         } else if needs_max_update {
